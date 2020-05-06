841b263d2e15f7b010d483bd8308db8dc50bf2a4
Jack Davis <jackd942@gmail.com>
2020-05-06 11:53:26 -0500

    wip - refactoring for protocol

--- lib/bob.ex
+++ lib/bob.ex
@@ -2,9 +2,27 @@ defmodule Bob do
   @type phrase :: String.t()
 
   @spec hey(phrase) :: String.t()
-  def hey(input) do
+  def hey(unstripped_input) do
+    input = String.strip(unstripped_input)
     cond do
-      true -> raise "Your implementation goes here"
+      silence?(input) -> "Fine. Be that way!"
+      shouting?(input) and question?(input) -> "Calm down, I know what I'm doing!"
+      shouting?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
+    
+      true -> "Whatever."
     end
+  end
+
+  def shouting?(input) do
+    (String.upcase(input) == input) and (String.downcase(input) != input)
+  end
+
+  def question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  def silence?(input) do
+    String.strip(input) == ""
   end
 end
